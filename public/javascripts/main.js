6f50e5b26f2b1e048fbd452f26d906753fb51ea7
John Doe <sylvie@Sylvies-MacBook-Pro.local>
2019-04-23 14:21:45 +0200

    123

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,27 +11,26 @@ function startMap() {
     // const contentString = <div id="content" />;
     //  var infowindow = new google.maps.InfoWindow({
     //content: contentString
-};
-
-    //MARKERS
-
-    const myMarker = new google.maps.Marker({
-        position: {
-            lat: 52.497992,
-            lng: 13.420135 //--->AJAX call
-        },
-        map: map,
-        title: "I'm here",
-        icon: './images/guinea-icon.jpg'
-    });
-    const myMarker2 = new google.maps.Marker({
-        position: {
-            lat: 45.3977381, //req.body.longitude --> array: dann wird die Position zu index 0 und 1as  qQ  Q
-            lng: 22.190471916 //req.body.latitude
-        },
-        map: map,
-        title: "I'm not here"
-    });
 }
+
+//MARKERS
+
+const myMarker = new google.maps.Marker({
+    position: {
+        lat: 52.497992,
+        lng: 13.420135 //--->AJAX call
+    },
+    map: map,
+    title: "I'm here",
+    icon: './images/guinea-icon.jpg'
+});
+const myMarker2 = new google.maps.Marker({
+    position: {
+        lat: 45.3977381, //req.body.longitude --> array: dann wird die Position zu index 0 und 1as  qQ  Q
+        lng: 22.190471916 //req.body.latitude
+    },
+    map: map,
+    title: "I'm not here"
+});
 
 startMap();
