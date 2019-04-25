d67e1a55e431d1757057ae2cfdc91c6ecdc6aed8
John Doe <sylvie@Sylvies-MacBook-Pro.local>
2019-04-25 12:48:33 +0200

    responsive version

--- bin/seeds.js
+++ bin/seeds.js
@@ -11,7 +11,7 @@ const Pet = require('../models/Pet');
 const bcryptSalt = 10;
 
 mongoose
-    .connect('mongodb://localhost/guinea-pig-match-app', { useNewUrlParser: true })
+    .connect(PROCESS.ENV.MONGODB.URI, { useNewUrlParser: true })
     .then(x => {
         console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
     })
